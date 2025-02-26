3b97b941fbef12bebcdf6cbdeb2517a1252bac27
SamG185 <105516431+SamG185@users.noreply.github.com>
2025-02-26 18:25:01 -0500

    Day 1 part 2 upload

---
+++ Part1.cs
@@ -0,0 +1,49 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Collections;
+
+StreamReader sr = new StreamReader("C:\\Users\\samue\\Documents\\Programming\\AOC_2024\\AOCDay1\\input\\input.txt");
+
+String line = sr.ReadLine();
+string[] stringsplit;
+ArrayList leftside = new ArrayList();
+Dictionary<string, int> rightSide = new Dictionary<string,int>();
+
+int total = 0;
+
+
+while (line != null)
+{
+    stringsplit = line.Split("   ");
+    leftside.Add(stringsplit[0]);
+    if(rightSide.ContainsKey(stringsplit[1]))
+    {
+        rightSide[stringsplit[1]]++;
+
+        Console.WriteLine("True Right");
+    }
+    else{
+        rightSide.Add(stringsplit[1], 1);
+    }
+
+    line = sr.ReadLine();
+}
+
+
+
+foreach(string numbers in leftside)
+{
+    if(rightSide.ContainsKey(numbers)){
+        total += rightSide[numbers] * int.Parse(numbers);
+    }
+}
+
+
+Console.WriteLine(total);
+
+sr.Close();
+
+
+
+
+
+
